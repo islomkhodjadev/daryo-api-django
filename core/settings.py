dd6426ece0879363b9f0c59097acd37c640f98cb
islomkhodjadev <islomkhodjadev@gmail.com>
2024-10-12 18:25:58 +0000

    version before adding token for usage limit

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9c1&76jsuxjf^=h$9-8$&9)1$(g%1j^bh0mfvf6^g*b0)j1m1f"
 
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["176.98.237.4", "127.0.0.1:8000", "127.0.0.1", "ringai.uz", 'www.ringai.uz']
 
 
